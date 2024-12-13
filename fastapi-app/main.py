9373e1654c57ff21a1c64ad121a360628330b756
Vlad-Note <gricenkovlad007@gmail.com>
2024-12-13 15:09:49 +0300

    Create main setting

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from api import router as api_router
+from core.config import Settings
 
 app = FastAPI()
+app.include_router(api_router, prefix="/api")
 
 
 if __name__ == "__main__":

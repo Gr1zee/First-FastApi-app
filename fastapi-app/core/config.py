c3493dac3ffe1559405da85d4237795729b4b44a
Vlad-Note <gricenkovlad007@gmail.com>
2024-12-16 14:11:57 +0300

    Fix db connect

--- fastapi-app/core/config.py
+++ fastapi-app/core/config.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
 
-class RunConfig(BaseModel):
+class RunConfig(BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8000
     
     
-class DataBaseConfig(BaseModel):
-    url: PostgresDsn
-    echo: bool=False, 
-    echo_pool: bool = False,
-    pool_size: int = 50,
+class DataBaseConfig(BaseSettings):
+    url: str
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
     max_overflow: int = 10    
 
 class Settings(BaseSettings):
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
         env_file=".env",
         case_sensitive=False,
         env_nested_delimiter="__",
-        ev_prefix="APP_CONFIG__"
+        env_prefix="APP_CONFIG__"
     )
     run: RunConfig = RunConfig()
     db: DataBaseConfig

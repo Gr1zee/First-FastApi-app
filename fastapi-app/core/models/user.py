a4271ef5a90cb33e44f366ce3525061996fdfd62
Vlad-Note <gricenkovlad007@gmail.com>
2025-01-30 12:51:53 +0300

    Recreate user model

--- fastapi-app/core/models/user.py
+++ fastapi-app/core/models/user.py
@@ -1,6 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from .base import Base
 
-class User(Base):
-    username: Mapped[str] = mapped_column(unique=True)
+
+class User(SQLAlchemyBaseUserTable[int]):
+    __tablename__ = "users"
